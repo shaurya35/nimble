c0a1293044ca0549e2c66769942ab43524218d64
Shaurya <shauryajha35@gmail.com>
2025-11-04 17:02:46 +0530

    Add Redirect

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>hi there</div>
-  );
+  redirect('/dashboard');
+  return null;
 }
